a0b18675eecf2ba64c102602b505175fff4e9c1a
scodfield <953836918@qq.com>
2024-11-13 19:20:43 +0800

    stun public ip

--- stun/client.go
+++ stun/client.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-func StunClient() {
+func StunClient() int {
 	udpServer, err := net.ResolveUDPAddr("udp", ":3478")
 	if err != nil {
 		log.Fatal(err)
@@ -19,36 +19,96 @@ func StunClient() {
 	}
 	defer conn.Close()
 
-	_, _, _ = stepPing(conn)
-}
-
-// stepPing: ping 检测
-// Return:
-// ping -- 能够进行udp通信
-// public -- 具有公网IP 或 对称型udp
-// nat -- nat网关, 需进一步判断nat类型
-func stepPing(conn *net.UDPConn) (ping, public, nat bool) {
-	reqMsg := &StunMsg{
-		ReqType: STUN_REQ_TYPE_PING,
-	}
-	reqBytes, err := json.Marshal(reqMsg)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	sendFailCount, timeoutCount, sameCount := 0, 0, 0
-	received := make([]byte, 1024)
 	err = conn.SetReadDeadline(time.Now().Add(5 * time.Second))
 	if err != nil {
 		log.Println("Error setting read deadline:", err)
 	}
+
+	received := make([]byte, 0, 1024)
+
+	_, isPublic, isNat := stepPing(conn, received)
+	if isPublic {
+		if stepPublic(conn, received) {
+			return NAT_TYPE_PUBLIC
+		}
+		return NAT_TYPE_SYMMETRY_FIREWALL
+	}
+
+	if isNat {
+		stepNat(conn, received)
+	}
+	return NAT_TYPE_NONE
+}
+
+// stepNat: NAT类型检测
+// @return true 完全锥形NAT
+// @return false 需进一步检查具体NAT类型
+func stepNat(conn *net.UDPConn, received []byte) bool {
+	reqBytes, err := json.Marshal(&StunMsg{
+		ReqType: STUN_REQ_TYPE_FULLCONE_NAT,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, timeoutCount, _ := commonSend(conn, reqBytes, received); timeoutCount >= 3 {
+		return false
+	}
+	return true
+}
+
+// stepPublic: public 检测
+// @return true 公网IP 且 无防火墙
+// @return false 对称型udp防火墙
+func stepPublic(conn *net.UDPConn, received []byte) bool {
+	reqBytes, err := json.Marshal(&StunMsg{
+		ReqType: STUN_REQ_TYPE_PUBLIC,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if _, timeoutCount, _ := commonSend(conn, reqBytes, received); timeoutCount >= 3 {
+		return false
+	}
+	return true
+}
+
+// stepPing: ping 检测
+// @return ping=true 能够进行udp通信
+// @return public=true 具有公网IP 或 对称型udp
+// @return nat=true nat网关, 需进一步判断nat类型
+func stepPing(conn *net.UDPConn, received []byte) (ping, public, nat bool) {
+	reqBytes, err := json.Marshal(&StunMsg{
+		ReqType: STUN_REQ_TYPE_PING,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sendFailCount, timeoutCount, sameCount := commonSend(conn, reqBytes, received)
+	if sendFailCount <= 0 {
+		ping = true
+	}
+	if sendFailCount <= 0 && timeoutCount <= 0 {
+		if sameCount >= 3 {
+			public = true
+		} else {
+			nat = true
+		}
+	}
+
+	return
+}
+
+func commonSend(conn *net.UDPConn, msg []byte, received []byte) (sendFailCount, timeoutCount, sameCount int) {
 	for i := 0; i < 3; i++ {
-		_, err = conn.Write(reqBytes)
+		_, err := conn.Write(msg)
 		if err != nil {
 			sendFailCount++
 			continue
 		}
 
+		received = received[0:0]
 		n, err := conn.Read(received)
 		if err != nil {
 			var netError net.Error
@@ -63,16 +123,5 @@ func stepPing(conn *net.UDPConn) (ping, public, nat bool) {
 			sameCount++
 		}
 	}
-	if sendFailCount <= 0 {
-		ping = true
-	}
-	if sendFailCount <= 0 && timeoutCount <= 0 {
-		if sameCount >= 3 {
-			public = true
-		} else {
-			nat = true
-		}
-	}
-
 	return
 }
--- stun/server.go
+++ stun/server.go
@@ -7,8 +7,16 @@ import (
 )
 
 const (
-	STUN_REQ_TYPE_PING     = iota // PING测试
-	STUN_REQ_TYPE_FIREWALL        // 检测客户端防火墙
+	STUN_REQ_TYPE_PING         = iota // PING测试
+	STUN_REQ_TYPE_PUBLIC              // 检测公网IP
+	STUN_REQ_TYPE_FULLCONE_NAT        // 检测完全锥形NAT
+)
+
+const (
+	NAT_TYPE_NONE              = iota
+	NAT_TYPE_PUBLIC            // 公网IP 且 没有防火墙
+	NAT_TYPE_SYMMETRY_FIREWALL // 对称型UDP防火墙
+	NAT_TYPE_FULLCONE_NAT      // 完全锥形NAT
 )
 
 type StunMsg struct {
@@ -22,6 +30,13 @@ func StunServer() {
 		log.Fatal(err)
 	}
 	defer udpServer.Close()
+
+	newUdpServer, err := net.ListenPacket("udp", ":3479")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer newUdpServer.Close()
+
 	for {
 		buf := make([]byte, 1024)
 		n, addr, err := udpServer.ReadFrom(buf)
@@ -29,14 +44,42 @@ func StunServer() {
 			log.Printf("Error reading from STUN server: %v", err)
 			continue
 		}
-		go response(udpServer, addr, buf[:n])
+		go response(udpServer, newUdpServer, addr, buf[:n])
 	}
 }
 
-func response(udpServer net.PacketConn, addr net.Addr, msg []byte) {
+func response(udpServer, newUdpServer net.PacketConn, addr net.Addr, msg []byte) {
+	reqMsg := &StunMsg{}
+	err := json.Unmarshal(msg, reqMsg)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	switch reqMsg.ReqType {
+	case STUN_REQ_TYPE_PING:
+		echoResponse(udpServer, addr)
+	case STUN_REQ_TYPE_PUBLIC:
+		changeIPAndPORTResponse(newUdpServer, addr)
+	case STUN_REQ_TYPE_FULLCONE_NAT:
+		changeIPAndPORTResponse(newUdpServer, addr)
+	}
+}
+
+func echoResponse(udpServer net.PacketConn, addr net.Addr) {
 	resMsg := StunMsg{
 		Addr: addr.String(),
 	}
 	res, _ := json.Marshal(resMsg)
 	udpServer.WriteTo(res, addr)
+}
+
+func changeIPAndPORTResponse(newUdpServer net.PacketConn, addr net.Addr) {
+	resMsg := StunMsg{
+		Addr: addr.String(),
+	}
+	res, _ := json.Marshal(resMsg)
+	_, err := newUdpServer.WriteTo(res, addr)
+	if err != nil {
+		log.Printf("Error writing from NewUdpServer, Error: %v\n", err)
+	}
 }
