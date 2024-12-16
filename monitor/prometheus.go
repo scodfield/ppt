78816f9463b3334a696a239f8d9cfac9bf85c674
scodfield <953836918@qq.com>
2024-12-16 21:24:44 +0800

    prometheus 1.0

--- monitor/prometheus.go
+++ monitor/prometheus.go
@@ -2,32 +2,50 @@ package monitor
 
 import (
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
+	"runtime"
 	"time"
 )
 
 var (
-	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "thd_op_count_test",
-		Help: "The total number of processed events",
+	counter = promauto.NewCounter(prometheus.CounterOpts{
+		Namespace: "actor2",
+		Name:      "thd_op_count_test",
+		Help:      "The total number of processed events",
+	})
+	gauge = promauto.NewGauge(prometheus.GaugeOpts{
+		Namespace: "actor2",
+		Name:      "thd_gauge_test",
+		Help:      "The total number of gauge",
 	})
 )
 
 // startPrometheus: 启动监控
 func StartPrometheus() {
+	reg := prometheus.NewRegistry()
+	initPromRegistry(reg)
+	promHandler := promhttp.HandlerFor(reg, promhttp.HandlerOpts{})
 	opProcessedMetrics()
 	server := http.NewServeMux()
-	server.Handle("/metrics", promhttp.Handler())
+	server.Handle("/metrics", promHandler)
 	http.ListenAndServe(":8081", server)
+}
+
+func initPromRegistry(reg *prometheus.Registry) {
+	reg.MustRegister(counter)
+	reg.MustRegister(gauge)
+	reg.MustRegister(collectors.NewGoCollector())
 }
 
 func opProcessedMetrics() {
 	go func() {
 		for {
-			opsProcessed.Inc()
-			time.Sleep(1 * time.Second)
+			counter.Inc()
+			gauge.Add(float64(runtime.NumGoroutine()))
+			time.Sleep(10 * time.Second)
 		}
 	}()
 }
