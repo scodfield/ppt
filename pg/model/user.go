17b44e5c3bda27b044a5ee004d8c1e09d657d7cf
scodfield <953836918@qq.com>
2025-02-15 20:15:22 +0800

    pgxpool batch insert

--- pg/model/user.go
+++ pg/model/user.go
@@ -1,12 +1,19 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"gorm.io/gorm"
+	"ppt/pg"
+)
 
 type User struct {
-	UserID   int64  `gorm:"primaryKey" json:"user_id"`
-	Username string `gorm:"size:255" json:"username"`
-	Password string `gorm:"size:255" json:"password"`
-	Email    string `gorm:"uniqueIndex;size:255" json:"email"`
+	ID       string `gorm:"type:uuid;primaryKey" json:"id"`
+	UserID   int64  `gorm:"not null;uniqueIndex;column:user_id;comment:玩家UserID" json:"user_id"`
+	Username string `gorm:"not null;size:255;column:user_name;comment:玩家名" json:"user_name"`
+	Password string `gorm:"not null;size:255;column:password;comment:密码" json:"password"`
+	Email    string `gorm:"not null;uniqueIndex;size:255;column:email;comment:注册邮箱" json:"email"`
 }
 
 func (User) TableName() string {
@@ -15,4 +22,41 @@ func (User) TableName() string {
 
 func MigrateUserModel(db *gorm.DB) error {
 	return db.AutoMigrate(&User{})
+}
+
+// CreateUser 创建用户
+func CreateUser(userID int64, userName, password, email string) (*User, error) {
+	uuidV7, err := uuid.NewV7()
+	if err != nil {
+		return nil, err
+	}
+	return &User{
+		ID:       uuidV7.String(),
+		UserID:   userID,
+		Username: userName,
+		Password: password,
+		Email:    email,
+	}, nil
+}
+
+// InsertUsersByPgxPool 批量插入用户数据
+func InsertUsersByPgxPool(pgxPool *pgxpool.Pool, users []*User) error {
+	userModel := User{}
+	batchSize := 1000
+	for i := 0; i < len(users); i += batchSize {
+		end := i + batchSize
+		if end > len(users) {
+			end = len(users)
+		}
+		usersBatch := users[i:end]
+		batch := &pgx.Batch{}
+		for _, user := range usersBatch {
+			batch.Queue(fmt.Sprintf(`insert into %s (id, user_id, user_name, password, emal) values ($1,$2,$3,$4,$5)`, userModel.TableName()), user.ID, user.UserID, user.Username, user.Password, user.Email)
+		}
+		err := pgxPool.SendBatch(pg.Ctx, batch).Close()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
