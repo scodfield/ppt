d52c66dd9ae9cb7a49f426e5024c31bbf771696d
scodfield <953836918@qq.com>
2025-04-08 22:13:21 +0800

    zap log core.Write

--- logger/zap.go
+++ logger/zap.go
@@ -12,6 +12,12 @@ func InitUberZap() error {
 	defer logger.Sync()
 
 	Logger = logger
-	
+
+	// 底层 cores.Write/2 方法同步写;
+	// 若自定义 core.Write/2 方法为异步写, 则在调用 buf.Free/0 方法前, 显式调用 core.Sync/0 方法 flush buffer;
+	// 或 make([]byte, len(buf.bs), len(buf.bs)), 将 buf 内容copy出来, 以免调用 buf.Free/0 释放 buf 后其它
+	// 协程持续引用该buf, 导致被后续日志覆写;
+	Logger.Info("init zap logger success")
+
 	return nil
 }
