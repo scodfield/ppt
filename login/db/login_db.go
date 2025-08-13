0a5e4b9f5f13bdeccea17fd483818dee38321539
scodfield <953836918@qq.com>
2025-08-13 21:33:18 +0800

    tidy

--- login/db/login_db.go
+++ login/db/login_db.go
@@ -1,51 +1,18 @@
 package db
 
 import (
-	"encoding/json"
 	"errors"
-	"fmt"
 	_ "github.com/astaxie/beego/cache"
 	_ "github.com/astaxie/beego/cache/redis"
-	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gomodule/redigo/redis"
 	"go.uber.org/zap"
-	redic "ppt/cache"
+	"ppt/cache"
 	"ppt/dao"
 	"ppt/logger"
-	"ppt/login/utils"
 	"ppt/model"
-	"strconv"
-	"sync"
 	"time"
 )
-
-var (
-	redisCache redic.Cache
-	o          orm.Ormer
-	count      int
-	countMutex sync.Mutex
-)
-
-const regHash = "user_reg"
-
-func init() {
-	// cache conn
-	redisCache, _ = redic.NewCache(`{"conn":"127.0.0.1:6379","password":"foobared"}`)
-
-	// init mysql
-	orm.RegisterModel(new(User))
-	orm.RegisterModel(new(LoginLog))
-	orm.RegisterDataBase("default", "mysql", "root:123456@tcp(127.0.0.1:3306)/thd_login?charset=utf8&loc=Local", 30)
-	orm.RunSyncdb("default", false, true)
-
-	// create orm instance
-	o = orm.NewOrm()
-
-	// init acc_id
-	curReg, _ := o.QueryTable("user").Count()
-	count = int(curReg)
-	fmt.Println("current count: ", count)
-}
 
 // SetAccountInfo 设置用户账号信息
 func SetAccountInfo(name string, user model.User) error {
@@ -73,85 +40,40 @@ func SetAccountInfo(name string, user model.User) error {
 	return nil
 }
 
-func GetAccountInfo(name string) interface{} {
-	userCache := GetLoginCache(name)
-	if userCache != nil {
-		return userCache
+// WhetherUserNameRegistered 用户Name是否已注册(true-已注册,false-未注册)
+func WhetherUserNameRegistered(name string) bool {
+	result := dao.RedisDB.HGet(dao.Ctx, dao.UserNameRegisterKey, name)
+	if result.Err() != nil {
+		if errors.Is(result.Err(), redis.ErrNil) {
+			return false
+		}
+		logger.Error("WhetherUserNameRegistered redis HGet error", zap.String("user_name", name), zap.Error(result.Err()))
+		return true
 	}
-
-	user := User{Name: name}
-	readErr := o.Read(&user, "Name")
-	if readErr != nil {
-		fmt.Println("get account info err: ", readErr)
-		return nil
+	regMilli, err := result.Int64()
+	if err != nil {
+		logger.Error("WhetherUserNameRegistered value assert error", zap.String("result_value", result.Val()), zap.Error(err))
+		return true
 	}
-	return user
-}
-
-func get_account_id() int {
-	countMutex.Lock()
-	defer countMutex.Unlock()
-	count++
-	return count
-}
-
-// whether registered
-func WhetherRegistered(name string) bool {
-	regReply, regErr := redisCache.Command("HGET", regHash, name)
-	if regErr != nil {
-		fmt.Println("user register error: ", regErr)
-		return false
-	} else if regReply == nil {
-		return false
-	}
-	regArray := regReply.([]byte)
-	regStr := string(regArray)
-	if regStr == "1" {
+	if regMilli > 0 {
 		return true
 	}
 	return false
 }
 
-// user login cache
-func SetLoginCache(user User) error {
-	cacheDuration := 3600 * time.Second
-	name := user.Name
-	userMarshal, _ := json.Marshal(user)
-	cacheErr := redisCache.SetEx(name+":cache", userMarshal, cacheDuration)
-	if cacheErr != nil {
-		fmt.Println("set account cache error: ", cacheErr)
-		return cacheErr
-	}
+// SetUserCache 设置用户缓存
+func SetUserCache(user model.User) error {
+	cache.UserCache.Set(user.UserID, user, dao.UserCacheDefaultExpiration)
 	return nil
 }
 
-// get login cache
-func GetLoginCache(name string) interface{} {
-	var userCache User
-	cacheRes := redisCache.Get(name + ":cache")
-	if cacheRes != nil {
-		cacheStr := cacheRes.([]byte)
-		json.Unmarshal(cacheStr, &userCache)
-		return userCache
+// GetUserCache 获取用户缓存
+func GetUserCache(userID uint64) (*model.User, error) {
+	userAny, err := cache.UserCache.Get(userID)
+	if err != nil {
+		logger.Error("GetUserCache Get user cache error", zap.Uint64("user_id", userID), zap.Error(err))
+		return nil, err
 	}
-	return nil
-}
-
-// login log
-func SetLoginLog(loginLog LoginLog) error {
-	if _, logErr := o.Insert(&loginLog); logErr != nil {
-		fmt.Println("loginLog err, ", logErr)
-		return logErr
-	}
-	// update sign info
-	time := loginLog.LoginTime
-	month := time.Month()
-	day := time.Day()
-	monthInd := utils.MonthMapping(month.String())
-	signKey := loginLog.Name + "_" + strconv.Itoa(monthInd)
-	redisCache.Command("SETBIT", signKey, day, 1)
-	// monthSign := redisCache.Get(signKey)
-	// daySign,_ := redisCache.Command("GETBIT", signKey, day)
-	// fmt.Println("month sign info: ", monthSign, ", daySign: ", daySign)
-	return nil
+	userCache := userAny.(model.User)
+	return &userCache, nil
 }
