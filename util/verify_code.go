b0b84bdc1e734a558bfa22742f0d6521deeb9af7
scodfield <953836918@qq.com>
2025-11-01 16:02:17 +0800

    feat：verify code 1.1

--- util/verify_code.go
+++ util/verify_code.go
@@ -5,6 +5,7 @@ import (
 	"golang.org/x/exp/rand"
 	"math/big"
 	"strconv"
+	"sync"
 	"time"
 )
 
@@ -24,4 +25,50 @@ func GenerateSecureDigitVerifyCode() string {
 	}
 	randNum := n.Int64() + 100000
 	return strconv.Itoa(int(randNum))
+}
+
+type VerifyCodeGenerator struct {
+	rnd  *rand.Rand
+	mu   sync.Mutex
+	pool sync.Pool
+}
+
+func NewVerifyCodeGenerator() *VerifyCodeGenerator {
+	g := &VerifyCodeGenerator{
+		rnd: rand.New(rand.NewSource(uint64(time.Now().UnixNano()))),
+	}
+	g.pool.New = func() interface{} {
+		return make([]byte, 6)
+	}
+	return g
+}
+
+func (g *VerifyCodeGenerator) Generate() string {
+	g.mu.Lock()
+	defer g.mu.Unlock()
+
+	buf := g.pool.Get().([]byte)
+	defer g.pool.Put(buf)
+
+	for i := range buf {
+		buf[i] = byte(g.rnd.Intn(10)) + '0'
+	}
+	return string(buf)
+}
+
+func (g *VerifyCodeGenerator) GenerateSecure() (string, error) {
+	g.mu.Lock()
+	defer g.mu.Unlock()
+
+	buf := g.pool.Get().([]byte)
+	defer g.pool.Put(buf)
+
+	for i := range buf {
+		randNum, err := crand.Int(crand.Reader, big.NewInt(10))
+		if err != nil {
+			return "", err
+		}
+		buf[i] = byte(randNum.Int64()) + '0'
+	}
+	return string(buf), nil
 }
